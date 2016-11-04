751f19b47e6eb49ee59702bb85eda9ef315a0bce
student5 <student3@jh-mba158-06.d214.org>
2016-11-04 09:41:09 -0500

    Added new view controller Added Segue's

---
+++ IntergalacticTravel/StarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StarViewController.swift
+//  IntergalacticTravel
+//
+//  Created by student3 on 11/3/16.
+//  Copyright © 2016 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
